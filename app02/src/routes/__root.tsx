e89d50114c1c7d5d4fbbd34218966332d0f93d0d
Leonardo Montini <lion.48m@gmail.com>
2024-04-11 22:10:08 +0200

    remote navigation from parent in sync

--- app02/src/routes/__root.tsx
+++ app02/src/routes/__root.tsx
@@ -1,10 +1,28 @@
-import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import {
+  Link,
+  Outlet,
+  createRootRoute,
+  useNavigate,
+} from "@tanstack/react-router";
+import { useEffect } from "react";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: Root,
+});
+
+function Root() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate({ to: window.location.pathname });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [window.location.pathname]);
+
+  return (
     <div>
       <Link to="/app02/foo">Foo</Link> - <Link to="/app02/bar">Bar</Link>
       <Outlet />
     </div>
-  ),
-});
+  );
+}
--- host/src/routes/__root.tsx
+++ host/src/routes/__root.tsx
@@ -1,19 +1,36 @@
-import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import {
+  Link,
+  Outlet,
+  createRootRoute,
+  useNavigate,
+} from "@tanstack/react-router";
+import { useEffect } from "react";
 
 export const Route = createRootRoute({
-  beforeLoad: () => {
-    console.log("Root Before Load");
-  },
-  component: () => (
+  component: Root,
+});
+
+function Root() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate({ to: window.location.pathname });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [window.location.pathname]);
+
+  return (
     <div>
       <div>Header</div>
       <div>
-        <Link to="/app01">App01</Link> | <Link to="/app02">App02</Link> |{" "}
-        <Link to="/app02/bar">App02-Bar</Link>
+        <Link to="/app01">App01</Link> | <Link to="/app02">App02</Link>
+        <button onClick={() => navigate({ to: "/app02/bar", state: true })}>
+          Bar
+        </button>
       </div>
       <div>
         <Outlet />
       </div>
     </div>
-  ),
-});
+  );
+}
