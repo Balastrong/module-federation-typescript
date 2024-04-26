aa299619d06d58a7721cf5b0d1afd28f215fda6f
Leonardo Montini <lion.48m@gmail.com>
2024-04-26 22:19:06 +0200

    redirect from app02 root

--- app02/src/routes/app02/index.tsx
+++ app02/src/routes/app02/index.tsx
@@ -1,5 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app02/")({
-  component: () => <div>Hello /app02/!</div>,
+  beforeLoad: () => {
+    throw redirect({
+      to: "/app02/foo",
+    });
+  },
 });
